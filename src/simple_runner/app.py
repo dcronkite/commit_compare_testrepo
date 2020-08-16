951a1577bf84d0e1c7134046e9c4acb7e15e77f6
David Cronkite <dcronkite@gmail.com>
2020-08-15 18:33:31 -0700

    Added string value

--- src/simple_runner/app.py
+++ src/simple_runner/app.py
@@ -1,14 +1,24 @@
 import sys
 import random
+import string
 
 
 def main(outpath):
-    header = ['id', 'val1', 'val2', 'val3', 'val4']
+    header = ['id', 'val1', 'val2', 'val3', 'val4', 'val5']
+    n_num = 4
+    n_str = 1
+    assert n_num + n_str + 1 == len(header)
+    str_options = ['STRONG'] * random.randint(1, 10) + \
+                  ['NORMAL'] * random.randint(1, 10) + \
+                  ['WEAK'] * random.randint(1, 10)
     with open(outpath, 'w') as out:
         out.write(','.join(header))
         out.write('\n')
         for row in range(100):
-            out.write(','.join(str(x) for x in [row] + [random.randint(0, 20) for col in header]))
+            out.write(','.join(str(x) for x in [row] +
+                               [random.randint(0, 20) for _ in range(n_num)] +
+                               [random.choice(str_options) for _ in range(n_str)]
+                               ))
             out.write('\n')
 
 
