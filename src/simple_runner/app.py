93ec3717e9398fa10deaff33ae1ae7135f31e055
David Cronkite <dcronkite@gmail.com>
2020-08-15 19:17:10 -0700

    Changed string values again: need 2 types of string

--- src/simple_runner/app.py
+++ src/simple_runner/app.py
@@ -7,9 +7,9 @@ def main(outpath):
     n_num = 4
     n_str = 1
     assert n_num + n_str + 1 == len(header)
-    str_options = ['STRONG'] * random.randint(1, 10) + \
-                  ['NORMAL'] * random.randint(5, 10) + \
-                  ['WEAK'] * random.randint(3, 10)
+    str_options = ['STRONG'] * random.randint(2, 10) + \
+                  ['NORMAL'] * random.randint(9, 10) + \
+                  ['WEAK'] * random.randint(5, 10)
     with open(outpath, 'w') as out:
         out.write(','.join(header))
         out.write('\n')
